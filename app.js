c88ce5eafb22fb85b3ac4c6a339daa161c8c8157
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-07 21:52:20 +0530

    #imported db, response and api testing

--- app.js
+++ app.js
@@ -1,13 +1,19 @@
-require('dotenv').config()
-const express = require('express')
-const cors = require('cors')
-const volleyball = require('volleyball')
-const app = express()
-app.use(cors())
-app.use(volleyball)
-app.use(express.json())
+require("dotenv").config();
+const express = require("express");
+const cors = require("cors");
+const volleyball = require("volleyball");
+const app = express();
+app.use(cors());
+app.use(volleyball);
+const db = require("./helpers/connection/connection");
+const response = require("./helpers/responses/response")
+app.use(express.json());
 
-app.listen(process.env.PORT, () => {
-    console.log(`Server listening on ${process.env.PORT}`)
+
+app.get('/', (req, res) => {
+    return response(res, 200, "API Testing", null)
 })
 
+app.listen(process.env.PORT, () => {
+  console.log(`Server listening on ${process.env.PORT}`);
+});
