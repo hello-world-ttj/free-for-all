86b7a6b1f3fca44dee7e6a1da8f5a374e3d40666
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-19 13:11:01 +0530

    #login function and jwt added

--- controllers/admin/auth/login.js
+++ controllers/admin/auth/login.js
@@ -1,12 +1,37 @@
 const jwt = require("jsonwebtoken");
-const { admin_login_schema_validation } = require("../../../helpers/validation/validation");
+const {
+  admin_login_schema_validation,
+} = require("../../../helpers/validation/validation");
 const admin_model = require("../../../models/admin.model");
-const response = require("../../../helpers/responses/response")
+const response = require("../../../helpers/responses/response");
+let md5 = require("md5");
 const admin_login_controller = async (req, res) => {
+  let admin_login_validator = admin_login_schema_validation.validate(req.body, {
+    abortEarly: true,
+  });
+  if (admin_login_validator.error)
+    return response(res, 400, "Invalid fields", admin_login_validator.error);
+  let admin_name = req.body.admin_name;
+  let admin_password = md5(req.body.admin_password);
   try {
-    let admin_name = req.body.admin_name;
-    let admin_password = req.body.admin_password;
-    let admin_login_validator = admin_login_jschema.validate(req.body, { abortEarly: true });
+    let find_admin = await admin_model.findOne({
+      admin_name: admin_name,
+      admin_password: admin_password,
+    });
+    if (find_admin) {
+      let payload = {
+        id: find_admin._id,
+      };
+      jwt.sign(payload, process.env.JWT_SECRET, async (error, token) => {
+        if (error) {
+          return response(res, 500, "Something went wrong while token generation", null);
+        } else {
+          return response(res, 200, "Login success", {token: token});
+        }
+      })
+    } else {
+      return response(res, 403, "Invalid Credentials", null);
+    }
   } catch (error) {
     console.log(error);
   }
