8fce68722822da95fa98bb477701ef02053981df
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-19 22:56:30 +0530

    #password

--- models/user.model.js
+++ models/user.model.js
@@ -3,6 +3,7 @@ const user_schema = new mongoose.Schema({
   user_first_name: { type: String },
   user_last_name: { type: String },
   user_name: { type: String },
+  user_password: { type: String },
   user_phone_number: { type: Number },
   user_email: { type: String },
   user_image: { type: String },
