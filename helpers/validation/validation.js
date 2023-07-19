4650432f6469a2020ad90bb677c30c69b2d8c6d0
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-19 22:57:12 +0530

    #edit user

--- helpers/validation/validation.js
+++ helpers/validation/validation.js
@@ -24,6 +24,15 @@ exports.registartion_schema_validation = Joi.object({
     user_password: Joi.string().required(),
     user_phone_number: Joi.number().min(10).required(),
     user_email: Joi.string().required(),
-    user_image: Joi.string().required(),
     user_dob: Joi.date().required(),
+})
+
+exports.edit_user_schema_validation = Joi.object({
+    user_first_name: Joi.string(),
+    user_last_name: Joi.string(),
+    user_name: Joi.string(),
+    user_phone_number: Joi.number().min(10),
+    user_email: Joi.string(),
+    user_image: Joi.string(),
+    user_dob: Joi.date(),
 })
