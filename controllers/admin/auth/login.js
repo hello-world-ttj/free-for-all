01d67f7a18d2e92c9bf37a858305ad50de45994d
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-19 23:00:42 +0530

    #admin signup, login

--- controllers/admin/auth/login.js
+++ controllers/admin/auth/login.js
@@ -4,7 +4,7 @@ const {
 } = require("../../../helpers/validation/validation");
 const admin_model = require("../../../models/admin.model");
 const response = require("../../../helpers/responses/response");
-let md5 = require("md5");
+const bcrypt = require("bcrypt");
 const admin_login_controller = async (req, res) => {
   let admin_login_validator = admin_login_schema_validation.validate(req.body, {
     abortEarly: true,
@@ -12,28 +12,36 @@ const admin_login_controller = async (req, res) => {
   if (admin_login_validator.error)
     return response(res, 400, "Invalid fields", admin_login_validator.error);
   let admin_name = req.body.admin_name;
-  let admin_password = md5(req.body.admin_password);
   try {
     let find_admin = await admin_model.findOne({
       admin_name: admin_name,
-      admin_password: admin_password,
     });
-    if (find_admin) {
+    let validate_admin = bcrypt.compare(
+      req.body.admin_password,
+      find_admin.admin_password
+    );
+    console.log(validate_admin);
+    if (validate_admin) {
       let payload = {
         id: find_admin._id,
       };
       jwt.sign(payload, process.env.JWT_SECRET, async (error, token) => {
         if (error) {
-          return response(res, 500, "Something went wrong while token generation", null);
+          return response(
+            res,
+            500,
+            "Something went wrong while token generation",
+            null
+          );
         } else {
-          return response(res, 200, "Login success", {token: token});
+          return response(res, 200, "Login success", { token: token });
         }
-      })
+      });
     } else {
       return response(res, 403, "Invalid Credentials", null);
     }
   } catch (error) {
-    console.log(error);
+    return response(res, 500, "Something Went Wrong", null);
   }
 };
 
--- controllers/admin/auth/signup.js
+++ controllers/admin/auth/signup.js
@@ -4,7 +4,7 @@ const {
 } = require("../../../helpers/validation/validation");
 const admin_model = require("../../../models/admin.model");
 const response = require("../../../helpers/responses/response");
-let md5 = require("md5");
+const bcrypt = require("bcrypt");
 const admin_signup_controller = async (req, res) => {
   try {
     let admin_signup_validator = admin_signup_schema_validation.validate(
@@ -13,11 +13,12 @@ const admin_signup_controller = async (req, res) => {
     );
     if (admin_signup_validator.error)
       return response(res, 400, "Invalid fields", admin_signup_validator.error);
+    const admin_password_hash = await bcrypt.hash(req.body.admin_password, 13);
     let data = {
       admin_first_name: req.body.admin_first_name,
       admin_last_name: req.body.admin_last_name,
       admin_name: req.body.admin_name,
-      admin_password: md5(req.body.admin_password),
+      admin_password: admin_password_hash,
     };
     let save_admin = await admin_model(data);
     let save_data = await save_admin.save();
