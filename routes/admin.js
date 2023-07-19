4e6f31d30ccc7ae18aeb09bc90f0e06bc49b900c
hello-world-ttj <tijotjoseph003@gmail.com>
2023-07-19 13:07:56 +0530

    #login

--- routes/admin.js
+++ routes/admin.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const admin_route = express.Router()
 const admin_signup_controller = require("../controllers/admin/auth/signup")
+const admin_login_controller = require("../controllers/admin/auth/login")
 admin_route.post("/signup", admin_signup_controller)
+admin_route.post("/login", admin_login_controller)
 
 module.exports = admin_route
